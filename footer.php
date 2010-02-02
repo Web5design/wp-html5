5df3c41d14704913b4bfa30cf9d0b6a8b2864f49
Trey Piepmeier <trey@treypiepmeier.com>
2010-02-01 19:52:55 -0600

    Started to convert to HTML5, removed Kubrick stuff.
    Theme screenshot.png is from http://diveintohtml5.org/

--- footer.php
+++ footer.php
@@ -1,22 +1,12 @@
-<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
-	<p>
-		<?php bloginfo('name'); ?> is proudly powered by
-		<a href="http://wordpress.org/">WordPress</a>
-		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
-		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
-		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
-	</p>
-
+		<footer>
+			<?php /*
+				<?php bloginfo('name'); ?> is proudly powered by
+				<a href="http://wordpress.org/">WordPress</a>
+			*/ ?>
+				<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+				<p>Copyright &copy; <?php echo date('Y'); ?> <a href="http://treypiepmeier.com/">Trey Piepmeier</a>. All rights reserved.</p>
+		</footer>
+	</section><!-- #wrapper -->
 	<?php wp_footer(); ?>
-	<script type="text/javascript">
-	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-	</script>
-	<script type="text/javascript">
-	try {
-	var pageTracker = _gat._getTracker("UA-97674-33");
-	pageTracker._trackPageview();
-	} catch(err) {}
-	</script>
 </body>
 </html>
--- single.php
+++ single.php
@@ -1,37 +1,31 @@
-<?php
-/**
- * @package WordPress
- * @subpackage Default_Theme
- */
+<?php get_header(); ?>
 
-get_header();
-?>
-
-	<div id="content" class="widecolumn" role="main">
+	<section id="content">
 
 	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<div class="navigation">
+		<nav>
 			<div class="alignleft"><?php previous_post_link('&laquo; %link') ?></div>
 			<div class="alignright"><?php next_post_link('%link &raquo;') ?></div>
-		</div>
+		</nav>
 
-		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<h2><?php the_title(); ?></h2>
 
 			<div class="entry">
+
 				<?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?>
 
+				<?php /*
 				<?php wp_link_pages(array('before' => '<p><strong>Pages:</strong> ', 'after' => '</p>', 'next_or_number' => 'number')); ?>
+
 				<?php the_tags( '<p>Tags: ', ', ', '</p>'); ?>
+
 
 				<p class="postmetadata alt">
 					<small>
 						This entry was posted
-						<?php /* This is commented, because it requires a little adjusting sometimes.
-							You'll need to download this plugin, and follow the instructions:
-							http://binarybonsai.com/wordpress/time-since/ */
-							/* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
+
 						on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
 						and is filed under <?php the_category(', ') ?>.
 						You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
@@ -56,9 +50,9 @@ get_header();
 
 					</small>
 				</p>
-
+				*/ ?>
 			</div>
-		</div>
+		</article>
 
 	<?php comments_template(); ?>
 
