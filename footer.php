eb9dca176f66f918f42fbd49171c7723e3bac3ea
Trey Piepmeier <trey@treypiepmeier.com>
2010-02-01 19:45:50 -0600

    Initial import of WordPress Default 1.6 by Michael Heilemann.

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<?php bloginfo('name'); ?> is proudly powered by
+		<a href="http://wordpress.org/">WordPress</a>
+		<br /><a href="<?php bloginfo('rss2_url'); ?>">Entries (RSS)</a>
+		and <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments (RSS)</a>.
+		<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</p>
+
+	<?php wp_footer(); ?>
+	<script type="text/javascript">
+	var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+	document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+	</script>
+	<script type="text/javascript">
+	try {
+	var pageTracker = _gat._getTracker("UA-97674-33");
+	pageTracker._trackPageview();
+	} catch(err) {}
+	</script>
+</body>
+</html>
